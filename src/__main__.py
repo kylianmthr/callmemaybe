49dd7f020ad90abd8f2abd9096a313998ecf5009
Kylian Mathurin <kmathuri@c1r3p9.42nice.fr>
2026-03-05 09:56:42 +0100

    Feature: Answering, Note: The model loop on his response

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,39 @@
 import sys
 from pydantic import ValidationError
 from src import parsing
-from src.stages import EncodingStage
+from src.llm_sdk import llm_sdk
+from src.stages import (
+    EncodingStage,
+    LogitsDict,
+    LogitsStage,
+    LogitsToWordStage,
+)
+import json
 
 
 def main(argv: list[str]) -> None:
     try:
+        llm = llm_sdk.Small_LLM_Model()
         functions = parsing.parse_json_object(
             parsing.file_to_functions_object(argv[1])
         )
         prompts = parsing.parse_prompts(
             parsing.file_to_prompts_object(argv[2])
         )
-        print(EncodingStage().process(prompts[0].content))
+        print("=== LLM ===")
+        print("- Prompt:", prompts[1])
+        sentence = "You must resume your responses."
+        sentence += prompts[1].content
+        while True:
+            logits = LogitsStage().process(
+                EncodingStage().process(sentence, llm), llm
+            )
+            max_logits = max(logits)
+            if max_logits == 151645:
+                break
+            word = LogitsToWordStage().process(logits, llm)
+            sentence += word
+            print(sentence)
     except (
         FileNotFoundError,
         PermissionError,
@@ -20,7 +41,7 @@ def main(argv: list[str]) -> None:
         ValidationError,
         KeyError,
     ) as e:
-        print(e)
+        print(type(e), e)
         exit()
 
 
