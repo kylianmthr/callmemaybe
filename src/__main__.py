1e5f42df68c41f63749cb20787deab1e84d97d29
Kylian Mathurin <kmathuri@c1r3p9.42nice.fr>
2026-03-06 16:13:15 +0100

    Feature: Prediction system finished. Note: Main have to be refactored (and add returns)

--- src/__main__.py
+++ src/__main__.py
@@ -20,13 +20,17 @@ def main(argv: list[str]) -> None:
         # functions = parsing.parse_json_object(
         #    parsing.file_to_functions_object(argv[1])
         # )
-        functions: list[str] = [
-            "fn_add_numbers",
-            "fn_greet",
-            "fn_reverse_string",
-            "fn_get_square_root",
-            "fn_substitute_string_with_regex",
-        ]
+        functions = {
+            "fn_add_numbers": ["a", "b"],
+            "fn_greet": ["name"],
+            "fn_reverse_string": ["s"],
+            "fn_get_square_root": ["a"],
+            "fn_substitute_string_with_regex": [
+                "source_string",
+                "regex",
+                "replacement",
+            ],
+        }
         prompts = parsing.parse_prompts(
             parsing.file_to_prompts_object(argv[2])
         )
@@ -34,33 +38,29 @@ def main(argv: list[str]) -> None:
         # for func in functions:
         #    allowed_logits += llm.encode(func).tolist()[0]
         # print(allowed_logits)
-        sentence = """You must generate a JSON response with this format:
+        initial_prompt = """You must generate a JSON response with this format:
 {
     "name": "function name",
-    "description": "description of the function",
-    "parameters": {
-      "something": {
-        "type": "number or string"
-      }
-    },
-    "returns": {
-      "type": "number or string"
-    }
+    "description": "description of the function"
 }
 So you have this keys:
 "name" (function name, You can choose from these functions:
 fn_add_numbers, fn_greet, fn_reverse_string, fn_get_square_root, fn_substitute_string_with_regex),
 "description" (description of the function),
-"parameters" (the parameters of the function.
-It must be objects with this format: \"something\": object, in this object, you have to include \"type\": \"the parameter type\". Type can be number or string.)
-returns (returns of fthe function.
-It must be objects with this format: \"type\": \"the return type\". Type can be number or string.)
 You can use the key only one time. You can't repeat the key. DO NOT REPEAT THE KEY. You must use all of the keys
 Prefer using , over \n if you want to add another key
 """
-        sentence += prompts[2].content
-        predict = JSONPredict()
+        initial_prompt += prompts[2].content
+        sentence = initial_prompt
+        predict = JSONPredict(
+            [
+                "name",
+                "description",
+            ]
+        )
         while True:
+            if not (len(predict.stack)):
+                break
             allowed_logits = []
             logits = LogitsStage().process(
                 EncodingStage().process(sentence, llm), llm
@@ -72,7 +72,7 @@ Prefer using , over \n if you want to add another key
                         allowed_logits += llm.encode(token).tolist()[0]
             word = DecodeStage().process(logits, llm, allowed_logits)
             print("==== FREE TEXT ====")
-            print(predict.actual_buffer.split(":")[-1] + word)
+            print(predict.last_key)
             if predict.get_state() == Status.FREE_TEXT and (
                 re.search(
                     r'([^" \t]+)"',
@@ -80,18 +80,54 @@ Prefer using , over \n if you want to add another key
                 )
             ):
                 word = word[: word.find('"') + 1]
-                print("===== OUI ======")
-            # print("\n========== DEBUG ==========")
+            print("\n========== DEBUG ==========")
             # print("=== Logits ===")
             # print("allowed:", allowed_logits)
-            # print("=== Stack ===")
-            # print("stack:", predict.stack)
+            print("=== Stack ===")
+            print("stack:", predict.stack)
             sentence += word
-            print(sentence)
             print("\n")
+            predict.manage_state(word)
+        generated_dictionnary = json.loads(
+            sentence.replace(initial_prompt, "")
+        )
+        generated_dictionnary["parameters"] = {}
+        predict = JSONPredict(functions[generated_dictionnary["name"]])
+        initial_prompt = f'You must generate a JSON response with this format:{{"parameter name": "type of the parameter", ...}} You will use the function {generated_dictionnary["name"]} with this parameters {functions[generated_dictionnary["name"]]}'
+        initial_prompt += prompts[2].content
+        sentence = initial_prompt
+        while True:
             if not (len(predict.stack)):
                 break
+            allowed_logits = []
+            logits = LogitsStage().process(
+                EncodingStage().process(sentence, llm), llm
+            )
+            possible_char = predict.get_possible_characters()
+            if len(predict.stack):
+                if possible_char:
+                    for token in possible_char:
+                        allowed_logits += llm.encode(token).tolist()[0]
+            word = DecodeStage().process(logits, llm, allowed_logits)
+            print("==== FREE TEXT ====")
+            print(predict.last_key)
+            print("==> predict.keys", predict.keys)
+            if predict.get_state() == Status.FREE_TEXT and (
+                re.search(
+                    r'([^" \t]+)"',
+                    predict.actual_buffer.split(":")[-1] + word,
+                )
+            ):
+                word = word[: word.find('"') + 1]
+            print("\n========== DEBUG ==========")
+            # print("=== Logits ===")
+            # print("allowed:", allowed_logits)
+            print("=== Stack ===")
+            print("stack:", predict.stack)
+            sentence += word
+            print("\n")
             predict.manage_state(word)
+        print(sentence.replace(initial_prompt, ""))
         # predict = JSONPredict()
         # print("stack:", predict.stack)
         # predict.get_input_status("{")
--- src/predict.py
+++ src/predict.py
@@ -9,12 +9,6 @@ class Status(enum.Enum):
     INSERT_KEY = [
         "name",
         "description",
-        "parameters",
-        "returns",
-        "type",
-        "s",
-        "a",
-        "b",
     ]
     INSERT_STRING = ['"']
     INSERT_SEMI_COLUMN = [":"]
@@ -24,9 +18,6 @@ class Status(enum.Enum):
         "fn_reverse_string",
         "fn_get_square_root",
         "fn_substitute_string_with_regex",
-        "string",
-        "number",
-        "source_string",
     ]
     INSERT_NL = [","]
     INSERT_ONLY_NL = [","]
@@ -35,30 +26,20 @@ class Status(enum.Enum):
 
 
 class JSONPredict:
-    def __init__(self) -> None:
+    def __init__(self, keys) -> None:
         self.stack: list[Status] = [Status.START]
         self.actual_buffer = ""
         self.last_key = ""
         self.model = Small_LLM_Model()
-        self.keys = [
-            "name",
-            "description",
-            "parameters",
-            "returns",
-            "type",
-            "s",
-            "a",
-            "b",
-        ]
+        self.keys = keys
         self.value = [
             "fn_add_numbers",
             "fn_greet",
             "fn_reverse_string",
             "fn_get_square_root",
             "fn_substitute_string_with_regex",
-            "string",
             "number",
-            "source_string",
+            "string",
         ]
 
     def append(self, status: Status) -> None:
@@ -96,7 +77,10 @@ class JSONPredict:
             r'"([^"]+)"\s*:\s*"([^"]+)"$', self.actual_buffer
         ):
             self.pop()
-            self.append(Status.INSERT_NL)
+            if len(self.keys):
+                self.append(Status.INSERT_NL)
+            else:
+                self.append(Status.END)
             return True
         if self.get_state() == Status.INSERT_STRING and re.search(
             r'"\w+"$', self.actual_buffer
@@ -122,10 +106,13 @@ class JSONPredict:
             r'"([^"]+)"\s*:\s*"([^"]+)"$', self.actual_buffer
         ):
             self.pop()
-            self.append(Status.INSERT_NL)
+            if len(self.keys):
+                self.append(Status.INSERT_NL)
+            else:
+                self.append(Status.END)
             return True
         if self.get_state() == Status.INSERT_VALUE:
-            for possibility in Status.INSERT_VALUE.value:
+            for possibility in self.value:
                 if possibility in self.actual_buffer:
                     self.pop()
                     self.append(Status.INSERT_STRING)
@@ -155,67 +142,7 @@ class JSONPredict:
             self.pop()
             self.append(Status.INSERT_KEY)
             return True
-        return False
-
-    def get_input_status(self, last_token: str, i: int = 0) -> bool:
-        if re.search("{", last_token):
-            print("1")
-            self.stack.pop()
-            self.stack.append(Status.INSERT_STRING)
+        if self.get_state() == Status.END:
+            self.pop()
             return True
-        elif re.search('"(.*?)"', last_token) and ":" not in last_token:
-            print("2")
-            self.stack.append(Status.INSERT_SEMI_COLUMN)
-            return True
-        elif re.search("}", last_token):
-            print("3")
-            self.stack.pop()
-            return True
-        elif (
-            re.search('"([^"]+)"\s*:', last_token)
-            and last_token[-1] == Status.INSERT_SEMI_COLUMN
-        ):
-            print("4")
-            self.stack.pop()
-            return True
-        elif re.search('"(description+)"\s*:$', last_token):
-            print("5")
-            self.stack.append(Status.FREE_TEXT)
-            return True
-        elif re.search('"([^"]+)"\s*:$', last_token):
-            print("6")
-            print("->", last_token)
-            self.stack.append(Status.INSERT_VALUE)
-            return True
-        elif re.search('"([^"]+)"\s*:\s*"([^"]+)"', last_token):
-            print("7")
-            self.stack.pop()
-            return True
-        elif i == 1:
-            return False
-        elif last_token == '"' and self.stack[-1] == Status.INSERT_STRING:
-            print("8")
-            self.stack.pop()
-            self.stack.append(Status.OBJECT)
-            self.actual_line += last_token
-            return True
-        elif re.search('"([^"]+)$', last_token):
-            self.stack.pop()
-            return True
-        elif re.search('"\w+', last_token):
-            self.stack.append(Status.OBJECT)
-            self.actual_line += last_token
-            return True
-        else:
-            self.actual_line += last_token
-            line_status = self.get_input_status(self.actual_line, 1)
-            print("Line:", self.actual_line)
-            if line_status:
-                if (
-                    self.stack[-1] != Status.INSERT_SEMI_COLUMN
-                    and self.stack[-1] != Status.INSERT_VALUE
-                    and self.stack[-1] != Status.FREE_TEXT
-                ):
-                    self.actual_line = ""
-                return True
         return False
